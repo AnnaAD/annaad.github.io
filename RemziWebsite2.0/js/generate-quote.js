1f174ece3ee4a51f53771122020d414b30e3fade
Anna AD <anna.arpacidusseau@gmail.com>
2019-06-24 13:42:51 -0500

    Update generate-quote.js

--- RemziWebsite2.0/js/generate-quote.js
+++ RemziWebsite2.0/js/generate-quote.js
@@ -8,12 +8,10 @@ $(document).ready(function(){
       author.push(quotes[i].split(" -")[1])
       quotes[i] = quotes[i].split(" -")[0]
     }
-console.log(author)
-console.log(quotes)
-console.log(rand);
+    
     var rand = Math.floor(Math.random()*quotes.length);
     $("#quote").text(quotes[rand])
-    $("#author").text(author[rand])
+    $("#author").text("-" +author[rand])
 
   });
 
