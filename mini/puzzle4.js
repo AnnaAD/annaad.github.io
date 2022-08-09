44f5e87380d7f0ef1c9cc797c3324cd1c2733266
AnnaAD <annaad@mit.edu>
2022-08-09 16:45:29 -0400

    switched the puzzle

---
+++ mini/puzzle4.js
@@ -0,0 +1,13 @@
+/*
+capeX
+oreal
+dingo
+Xucla
+Xmeed
+*/
+
+var puzzle = {
+    grid: [['c', 'a', 'p', 'e', 'X'], ['o', 'r', 'e', 'a', 'l'], ['d', 'i', 'n', 'g', 'o'], ['X', 'u', 'c', 'l', 'a'], ['X', 'm', 'e', 'e', 'd']],
+    acrossClues: ["with 1 down, the handle of the gun-shaped state ","french skin care brand, l'____","a ____ ate my baby!", "a school where the students might drive maseratis ","archaic, a deserved share"],
+    downClues: ["see 1 across","latin suffix following vivar- or solar-","mike ____, hates gay people","america's bird", "follows down or over"]
+  };
