887a1780ec656612c1b896794623a43b850613d6
AnnaAD <annaad@mit.edu>
2022-08-10 10:48:20 -0400

    mini update

---
+++ mini/puzzle5.js
@@ -0,0 +1,15 @@
+/*
+macsX
+aloof
+pinto
+lover
+enorm
+*/
+
+var puzzle = {
+    grid: [['m', 'a', 'c', 's', 'X'], ['a', 'l', 'o', 'o', 'f'], ['p', 'i', 'n', 't', 'o'], ['l', 'o', 'v', 'e', 'r'], ['e', 'n', 'o', 'r', 'm']],
+    acrossClues: ["pc competitors","cool and distant","white or wheat? white or brown? black or ____?", "TS's pastel album","prefix indicating size"],
+    downClues: ["toronto team mascot","","A talk, informally","Greek God or Spirit of Safety & Deliverance", "shape"]
+  };
+
+  playGame(puzzle);
